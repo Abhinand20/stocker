6ad59706ca4b69c19d9de78c806b06e051a7dc72
Abhinand Jha <jha.abhinand1@gmail.com>
2025-11-30 02:34:28 -0800

    Setup basic filing scraper with search and filters

---
+++ constants.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+class FilingType(Enum):
+    UNKNOWN = "unknown"
+    ANNUAL_REPORT = "annual_report"
+    PERIODIC_TRANSACTION_REPORT = "periodic_transaction_report"
+
+SENATE_FILING_SEARCH_REQUEST_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+    "Referer": "https://efdsearch.senate.gov/search/home/",
+    "Origin": "https://efdsearch.senate.gov",
+}
+
+REPORT_TYPE_TO_ID = {
+    FilingType.ANNUAL_REPORT: 7,
+    FilingType.PERIODIC_TRANSACTION_REPORT: 11,
+}
+
+SENATE_FILING_SEARCH_PARAMS = {
+    "draw": 1,
+    "columns[0][data]": "0",
+    "columns[0][name]": "",
+    "columns[0][searchable]": "true",
+    "columns[0][orderable]": "true",
+    "columns[0][search][value]": "",
+    "columns[0][search][regex]": "false",
+    "columns[1][data]": "1",
+    "columns[1][name]": "",
+    "columns[1][searchable]": "true",
+    "columns[1][orderable]": "true",
+    "columns[1][search][value]": "",
+    "columns[1][search][regex]": "false",
+    "columns[2][data]": "2",
+    "columns[2][name]": "",
+    "columns[2][searchable]": "true",
+    "columns[2][orderable]": "true",
+    "columns[2][search][value]": "",
+    "columns[2][search][regex]": "false",
+    "columns[3][data]": "3",
+    "columns[3][name]": "",
+    "columns[3][searchable]": "true",
+    "columns[3][orderable]": "true",
+    "columns[3][search][value]": "",
+    "columns[3][search][regex]": "false",
+    "columns[4][data]": "4",
+    "columns[4][name]": "",
+    "columns[4][searchable]": "true",
+    "columns[4][orderable]": "true",
+    "columns[4][search][value]": "",
+    "columns[4][search][regex]": "false",
+    "order[0][column]": "1",
+    "order[0][dir]": "asc",
+    "order[1][column]": "0",
+    "order[1][dir]": "asc",
+    "start": 0,
+    "length": "25",
+    "search[value]": "",
+    "search[regex]": "false",
+    "report_types": f"[{', '.join([str(REPORT_TYPE_TO_ID[filing_type]) for filing_type in FilingType if filing_type != FilingType.UNKNOWN])}]",
+    "filer_types": "[1]",
+    "submitted_start_date": "01/01/2012 00:00:00",
+    "submitted_end_date": "12/31/2023 23:59:59",
+    "candidate_state": "",
+    "senator_state": "",
+    "office_id": "",
+    "first_name": "",
+    "last_name": "",
+}
---
+++ scraper.py
@@ -0,0 +1,171 @@
+"""
+Two step scraper:
+1. Fetch all the filing URLs that need to be parsed in this run.
+2. Download the filings and store them for later parsing.
+"""
+
+from abc import ABC, abstractmethod
+from enum import Enum
+from dataclasses import dataclass
+from typing import List, Optional
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+import constants
+import requests
+import logging
+
+@dataclass(frozen=True)
+class FilingResult:
+    first_name: str
+    last_name: str
+    office_name: str
+    filing_date: datetime
+    filing_type: constants.FilingType
+    filing_url: str
+
+@dataclass
+class FilingSearchFilters:
+    filing_types: Optional[list[constants.FilingType]] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+
+    def to_search_params(self) -> dict:
+        search_params = {}
+        if self.filing_types:
+            report_types = [str(constants.REPORT_TYPE_TO_ID[filing_type]) for filing_type in self.filing_types if filing_type != constants.FilingType.UNKNOWN]
+            if report_types:
+                search_params['report_types'] = f"[{', '.join(report_types)}]"
+        if self.start_date:
+            search_params['submitted_start_date'] = self.start_date.strftime("%m/%d/%Y %H:%M:%S")
+        if self.end_date:
+            search_params['submitted_end_date'] = self.end_date.strftime("%m/%d/%Y %H:%M:%S")
+        return search_params
+
+def parse_filing_type(type_str: str) -> constants.FilingType:
+    if "annual report" in type_str.lower():
+        return constants.FilingType.ANNUAL_REPORT
+    elif "periodic transaction report" in type_str.lower():
+        return constants.FilingType.PERIODIC_TRANSACTION_REPORT
+    else:
+        logging.warning(f"Unknown filing type: {type_str}")
+        return constants.FilingType.UNKNOWN
+
+def parse_filing_results(results: list[list[str]]) -> list[FilingResult]:
+    all_results = []
+    url_pattern = re.compile(r'<a[^>]*href="([^"]+)"[^>]*>(.*?)</a>')
+    for row in results:
+        match = url_pattern.search(row[3])
+        if not match:
+            logging.warning(f"Failed to parse filing URL from {row}")
+            continue
+        filing_url = match.group(1)
+        filing_type = parse_filing_type(match.group(2))
+        filing_result = FilingResult(
+            first_name=row[0].strip(),
+            last_name=row[1].strip(),
+            office_name=row[2].strip(),
+            filing_date=datetime.strptime(row[4], "%m/%d/%Y"),
+            filing_type=filing_type,
+            filing_url=filing_url,
+        )
+        all_results.append(filing_result)
+    return all_results
+
+class Filing(ABC):
+    @abstractmethod
+    def get_url(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_content(self) -> bytes:
+        pass
+
+class FilingScraper(ABC):
+    @abstractmethod
+    def scrape_filing_urls(self, filters: Optional[FilingSearchFilters] = None) -> List[Filing]:
+        pass
+
+    @abstractmethod
+    def download_filing(self, filing: Filing) -> bytes:
+        pass
+
+# These types of filings are simpler to parse because they are already in a structured format.
+class FilingHTML(Filing):
+    def __init__(self, filing_result: FilingResult):
+        self.filing_result = filing_result
+
+    def get_url(self) -> str:
+        return self.filing_result.filing_url
+
+    def get_content(self) -> bytes:
+        return b""
+
+# These types of filings may need to be parsed using OCR.
+class FilingPDF(Filing):
+    def __init__(self, filing_result: FilingResult):
+        self.filing_result = filing_result
+
+    def get_url(self) -> str:
+        return self.filing_result.filing_url
+
+    def get_content(self) -> bytes:
+        return b""
+
+
+class SenateFilingScraper(FilingScraper):
+    def __init__(self, session: requests.Session):
+        self.session = session
+        self.root_url = "https://efdsearch.senate.gov/search/home/"
+        self.search_url = "https://efdsearch.senate.gov/search/report/data/"
+        self.search_params = constants.SENATE_FILING_SEARCH_PARAMS.copy()
+        self.search_headers = constants.SENATE_FILING_SEARCH_REQUEST_HEADERS.copy()
+        self.csrf_token = None
+
+    def scrape_filing_urls(self, filters: Optional[FilingSearchFilters] = None) -> List[FilingResult]:
+        # Fetch the ack token from the root page.
+        self.session.headers.update(self.search_headers)
+        if filters:
+            self.search_params.update(filters.to_search_params())
+        response = self.session.get(self.root_url)
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch root page: {response.status_code}")
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.csrf_token = soup.select_one('[name="csrfmiddlewaretoken"]')["value"]
+        self.session.headers.update({"X-CSRFToken": self.csrf_token})
+        logging.info(f"Fetched Root page and CSRF token successfully!")
+        all_data = []
+        while True:
+            response = self.session.post(self.search_url, data=self.search_params)
+            if response.status_code != 200:
+                raise Exception(f"Failed to search filings: {response.status_code}")
+            d = response.json()
+            if not d["data"]:
+                break
+            all_data.extend(parse_filing_results(d["data"]))
+            self.search_params["draw"] += 1
+            self.search_params["start"] += 25
+            break
+        logging.info(f"Fetched {len(all_data)} filings successfully!")
+        return all_data
+
+    def download_filing(self, filing: Filing) -> bytes:
+        return b""
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    session = requests.Session()
+    scraper = SenateFilingScraper(session)
+    filters = FilingSearchFilters(filing_types=[constants.FilingType.PERIODIC_TRANSACTION_REPORT], start_date=datetime(2025, 10, 1), end_date=datetime(2025, 10, 31))
+    try:
+        filings = scraper.scrape_filing_urls(filters)
+        for filing in filings:
+            print(filing)
+    except Exception as e:
+        logging.error(f"Failed to scrape filings: {e}")
+    finally:
+        session.close()
+
+if __name__ == "__main__":
+    main()
